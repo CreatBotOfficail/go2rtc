4e5de86e120acc8a9e2d576fb4cb565790ff86c5
zkk <1007518571@qq.com>
2025-05-20 17:06:31 +0800

    调整webrtc页面
    隐藏控件和模式标签

--- www/video-rtc.js
+++ www/video-rtc.js
@@ -238,7 +238,7 @@ export class VideoRTC extends HTMLElement {
      */
     oninit() {
         this.video = document.createElement('video');
-        this.video.controls = true;
+        this.video.controls = false;
         this.video.playsInline = true;
         this.video.preload = 'auto';
 
